5f771df4fc91d7564ff2c9fc7b85831abdc5861c
yangyuexiong <yang6333yyx@126.com>
2019-05-27 11:15:22 +0800

    update selenuim and add chromedriver

--- WFX_BusinessLogic_Test/common/myunit.py
+++ WFX_BusinessLogic_Test/common/myunit.py
@@ -9,6 +9,7 @@
 import unittest
 import logging
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
 
@@ -17,15 +18,29 @@ ps -ef | grep phantomjs
 killall phantomjs
 """
 
-
+"""
+ps -ef | grep chromedriver
+killall chromedriver
+"""
 class StartEnd(unittest.TestCase):
     driver_swicth = False
     # mac
     if driver_swicth:
+        """PhantomJ用于旧版selenuim"""
         # mac
-        driver = webdriver.PhantomJS(executable_path='../phantomjs-2.1.1-macosx/bin/phantomjs')
+        # driver = webdriver.PhantomJS(executable_path='../phantomjs-2.1.1-macosx/bin/phantomjs')
         # Windows
         # driver = webdriver.PhantomJS(executable_path=r'..\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+        """chromedriver无头用于新版selenuim"""
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+        chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+        chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+        chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+        chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        # chrome_options.binary_location = "./chromedriver_for_mac/chromedriver"  # 手动指定使用的浏览器位置
+        driver = webdriver.Chrome(executable_path='./chromedriver_for_mac/chromedriver', chrome_options=chrome_options)
 
     def setUp(self):
         print('======测试开始======')
